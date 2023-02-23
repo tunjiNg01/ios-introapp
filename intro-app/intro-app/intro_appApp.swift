bfa5747c35716d66f845d4fda59b2221192f0151
tunjiNg01 <nahtlosng@gmail.com>
2023-02-23 12:17:37 +0100

    init

---
+++ intro-app/intro-app/intro_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  intro_appApp.swift
+//  intro-app
+//
+//  Created by MACBOOK PRO on 23/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct intro_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
